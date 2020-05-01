9d74de450781f4eaf47be0c947fbb8d28b24f6e7
sjh938606 <sjh938606@gmail.com>
2020-05-01 07:52:01 +0000

    기본 구조 설계

---
+++ src/app/main/con/con.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-con',
+  templateUrl: './con.component.html',
+  styleUrls: ['./con.component.scss']
+})
+export class ConComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
